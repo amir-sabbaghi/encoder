4cff1589421eb2f276ffc4e0f9bee2056f5b01ea
Amir Sabbaghi <asaba90@gmail.com>
2016-09-02 12:06:34 +0430

    cleanly close pipes and processes

--- src/Main.hs
+++ src/Main.hs
@@ -9,6 +9,7 @@ import Options.Applicative hiding (command)
 import System.Process
 import qualified Data.ByteString as BS
 import Control.Concurrent (forkIO)
+import System.Exit
 
 import Packet
 
@@ -92,7 +93,11 @@ client host port = connect host port $ \(socket, addr) -> do
                                                  forkIO $ let loop = do bs <- BS.hGetSome outh 65535
                                                                         putStrLn $ "sending " ++ show (BS.length bs) ++ " bytes"
                                                                         if BS.null bs then do
-                                                                          send s $ packet output BS.empty
+                                                                          exitCode <- waitForProcess ph
+                                                                          case exitCode of
+                                                                            ExitSuccess -> send s $ packet output BS.empty
+                                                                            ExitFailure code -> do putStrLn $ "ffmpeg exited with exit code: " ++ show code
+                                                                                                   error "ffmpeg exited abnormally"
                                                                         else do
                                                                           send s $ packet output bs
                                                                           loop
@@ -104,7 +109,11 @@ client host port = connect host port $ \(socket, addr) -> do
                                                  dispatch s newm
                               Just (i, bs) -> case lookup i m of
                                                 Nothing -> putStrLn $ "Cannot find index " ++ show i
-                                                Just list -> do let put = flip BS.hPut bs
-                                                                putStrLn $ "Converting " ++ show (BS.length bs) ++ " bytes"
-                                                                mapM_ put $ map snd list
-                                                                dispatch s m
+                                                Just list -> if BS.null bs then do
+                                                               putStrLn $ "Closing pipes"
+                                                               mapM_ hClose $ map snd list
+                                                               dispatch s $ filter (\(j, _) -> j /= i) m
+                                                             else do let put = flip BS.hPut bs
+                                                                     putStrLn $ "Converting " ++ show (BS.length bs) ++ " bytes"
+                                                                     mapM_ put $ map snd list
+                                                                     dispatch s m
