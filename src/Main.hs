e22434fd9541c0a8cdb7a9a0449a352776c800f5
Amir Sabbaghi <asaba90@gmail.com>
2016-09-04 23:37:49 +0430

    initial support for management server

--- src/Main.hs
+++ src/Main.hs
@@ -1,6 +1,7 @@
 module Main where
 
 import Options.Applicative hiding (command)
+import System.Exit
 
 import Client
 import Server
@@ -8,6 +9,7 @@ import Server
 data Flags = Flags
     { flagPort :: String
     , flagServer :: Bool
+    , flagManagementPort :: Maybe String
     , flagClient :: Maybe String
 --    , flagCert :: String
 --    , flagKey  :: String
@@ -17,6 +19,7 @@ parseFlags :: Parser Flags
 parseFlags = Flags
         <$> strOption (short 'p' <> long "port" <> metavar "PORT" <> help "The port to listen for incoming connections/or connect to server in case of client")
         <*> switch (short 's' <> long "server" <> help "Act as server")
+        <*> optional (strOption (short 'm' <> long "management" <> metavar "PORT" <> help "The port to send encoding requests to"))
         <*> optional (strOption (short 'c' <> long "client" <> metavar "SERVER" <> help "Act as client"))
 --        <*> strOption (long "cert" <> metavar "CERTFILE" <> help "Server's certificate file path")
 --        <*> strOption (long "key" <> metavar "KEYFILE" <> help "Server's private key file path")
@@ -29,8 +32,8 @@ main = execParser opts >>= decide
                )
 
 decide :: Flags -> IO ()
-decide (Flags port True Nothing) = server port
-decide (Flags port False (Just h)) = client h port
-decide (Flags _    False Nothing) = error "One of -s or -c should be specified"
-decide (Flags _    True _) = error "Only one of -s or -c should be specified"
-
+decide (Flags port True  (Just mport) Nothing) = server port mport
+decide (Flags port False Nothing      (Just h)) = client h port
+decide (Flags _    True  Nothing      Nothing) = die "Management port should be specified with -m"
+decide (Flags _    False _            Nothing) = die "One of -s or -c should be specified"
+decide (Flags _    True  _            _) = die "Only one of -s or -c should be specified"
--- src/Server.hs
+++ src/Server.hs
@@ -3,31 +3,87 @@ module Server ( server
 
 import Network.Simple.TCP
 import qualified Data.ByteString as BS
-import Control.Concurrent (forkIO)
+import qualified Data.ByteString.Char8 as BSC (unpack)
+import Control.Concurrent
+import Control.Concurrent.STM.TQueue
+import Control.Monad
+import Control.Monad.STM (atomically)
+import Control.Exception
+import System.Process
+import System.Directory
+import System.Exit
+import GHC.IO.Handle
 
 import Packet
 
-server :: String -> IO ()
-server port = do let codes = ["-map 0:0 -c:v libx265 -x265-params crf=25 -map_metadata -1 -metadata:s:0 language=en"
-                              ,"-map 0:0 -c:v libx265 -x265-params crf=25 -map_metadata -1 -metadata:s:0 language=en -s 1280x720"
-                              ,"-map 0:1 -c:a aac -ac 2 -b:a 128k -strict -2 -map_metadata -1 -metadata:s:0 language=en"
-                              ]
-                 serve HostAny port $ \(socket, remoteAddr) -> do
-                     mapM_ (send socket) $ zipWith (command 1) [1..] codes
-                     forkIO $ let loop bs = if BS.length bs < 65535 then do
-                                              send socket $ packet 1 bs
-                                              send socket $ packet 1 BS.empty
-                                            else do
-                                              let (b, bss) = BS.splitAt 65535 bs
-                                              send socket $ packet 1 b
-                                              loop bss
-                              in do
-                                film <- BS.getContents
-                                loop film
-                     let loop = do pckt <- readPacket socket
-                                   case pckt of
-                                     Nothing -> return ()
-                                     Just (i, bs) -> do BS.appendFile (show i) bs
-                                                        loop
-                     loop
+codes = [ "-map 0:0 -c:v libx265 -x265-params crf=25 -map_metadata -1 -metadata:s:0 language=en"
+        , "-map 0:0 -c:v libx265 -x265-params crf=25 -map_metadata -1 -metadata:s:0 language=en -s 1280x720"
+        , "-map 0:1 -c:a aac -ac 2 -b:a 128k -strict -2 -map_metadata -1 -metadata:s:0 language=en"
+        ]
 
+server :: String -> String -> IO ()
+server port mport = do todo <- atomically $ newTQueue
+                       done <- atomically $ newTQueue
+                       film <- atomically $ newTQueue
+                       forkIO $ managementServer mport todo film
+                       forkIO $ mergeServer done film
+                       queueServer port todo done
+
+managementServer :: String -> TQueue (FilePath, Int, String) -> TQueue (String, Int) -> IO ()
+managementServer port todo film = serve HostAny port $ \(socket, remoteAddr) ->
+  do bs <- recvAll socket
+     let [path,name] = read $ BSC.unpack bs
+     print $ [path,name]
+     mins <- totalMinutes path
+     atomically $ mapM_ (\i -> writeTQueue todo (path, i, name)) [0..mins]
+     atomically $ writeTQueue film (path, mins+1)
+     where recvAll s = do mbs <- recv s 1024
+                          case mbs of
+                            Nothing -> return BS.empty
+                            Just bs -> recvAll s >>= (return . BS.append bs)
+           totalMinutes :: FilePath -> IO Int
+           totalMinutes path = do let args = ["-show_entries","format=duration","-of","default=noprint_wrappers=1:nokey=1",path]
+                                  (exitCode, out, err) <- readProcessWithExitCode "/usr/bin/ffprobe" args ""
+                                  assert (exitCode == ExitSuccess) $ return . ceiling . read $ out
+
+queueServer :: String -> TQueue (FilePath, Int, String) -> TQueue (String, Int) -> IO ()
+queueServer port todo done = serve HostAny port $ \(socket, remoteAddr) ->
+  do putStrLn $ "Worker connected from " ++ show remoteAddr
+     worker socket remoteAddr
+  where worker socket remoteAddr =
+          do (path, min, name) <- atomically $ readTQueue todo
+             mapM_ (send socket) $ zipWith (command 1) [1..] codes
+             mapM_ (try . removeFile :: FilePath -> IO (Either IOException ())) $ map ((name ++).("_" ++).show) [1..length codes]
+             bracketOnError (ffmpegProc path min)
+                            (clean todo (path, min, name) . snd)
+                            (transfer name socket . fst)
+             atomically $ writeTQueue done (name, min)
+             worker socket remoteAddr
+        ffmpegProc :: FilePath -> Int -> IO (Handle, ProcessHandle)
+        ffmpegProc path min =
+          do let args = ["-ss",show min++":00","-t","1:00","-i",path,"-c","copy","-f","matroska","pipe:1"]
+                 ffmpeg = (proc "/usr/bin/ffmpeg" args) { std_out = CreatePipe }
+             (Nothing, Just outh, Nothing, ph) <- createProcess ffmpeg
+             return (outh, ph)
+        clean todo t h = do atomically $ unGetTQueue todo t
+                            terminateProcess h
+        transfer :: String -> Socket -> Handle -> IO ()
+        transfer name s h =
+          do tid <- myThreadId
+             forkIO $ handle (throwTo tid :: SomeException -> IO ()) $
+                             while $ do bs <- BS.hGetSome h 65535
+                                        if BS.null bs then
+                                          send s (packet 1 BS.empty) >> return False
+                                        else
+                                          send s (packet 1 bs) >> return True
+             while $ do mbs <- readPacket s
+                        case mbs of
+                          Nothing -> return False
+                          Just (i, bs) -> BS.appendFile (name ++ "-" ++ show i) bs >> return True
+
+mergeServer :: TQueue (String, Int) -> TQueue (String, Int) -> IO ()
+mergeServer done number = return ()
+
+while :: IO Bool -> IO ()
+while f = do r <- f
+             when r $ while f
